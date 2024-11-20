b054bfe180f072b4d824ec3bc2af50c13ea352c0
Jonathan Timbaldi <7434335+imatimba@users.noreply.github.com>
2024-11-20 20:57:01 -0300

    Add .mozilla/firefox/default-release/user.js

---
+++ private_dot_mozilla/private_firefox/private_default-release/user.js
@@ -0,0 +1,8 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("gfx.webrender.enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("browser.urlbar.clickSelectsAll", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("browser.compactmode.show", true);
